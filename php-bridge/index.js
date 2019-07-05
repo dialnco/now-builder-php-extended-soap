eb6c5cd014f4bd2b9ed629cf2e9a1e146ebf268e
Dial Cortez <dialnco@gmail.com>
2019-07-05 01:35:46 -0400

    added dependencies

--- php-bridge/index.js
+++ php-bridge/index.js
@@ -7,9 +7,12 @@ const fs = require('fs');
 
 async function patchLauncher({ documentRoot }) {
   const launcher = await FileBlob.fromStream({
-    stream: new FileFsRef({ fsPath: path.join(__dirname, 'launcher.js') }).toStream(),
+    stream: new FileFsRef({
+      fsPath: path.join(__dirname, 'launcher.js')
+    }).toStream()
   });
-  launcher.data = launcher.data.toString()
+  launcher.data = launcher.data
+    .toString()
     .replace(/\/var\/task\/user/g, path.join('/var/task/user', documentRoot));
   return launcher;
 }
@@ -18,17 +21,22 @@ function spawnAsync(command, args, cwd) {
   return new Promise((resolve, reject) => {
     const child = spawn(command, args, { stdio: 'inherit', cwd });
     child.on('error', reject);
-    child.on('close', (code, signal) => (code !== 0
-      ? reject(new Error(`Exited with ${code || signal}`))
-      : resolve()));
+    child.on('close', (code, signal) =>
+      code !== 0
+        ? reject(new Error(`Exited with ${code || signal}`))
+        : resolve()
+    );
   });
 }
 
 async function getFiles({ documentRoot = '' }) {
   const files = await glob('native/**', __dirname);
 
-  const phpConfig = await FileBlob.fromStream({ stream: files['native/php.ini'].toStream() });
-  phpConfig.data = phpConfig.data.toString()
+  const phpConfig = await FileBlob.fromStream({
+    stream: files['native/php.ini'].toStream()
+  });
+  phpConfig.data = phpConfig.data
+    .toString()
     .replace(/\/root\/app\/native\/modules/g, '/var/task/native/modules');
   files['native/php.ini'] = phpConfig;
 
@@ -36,11 +44,21 @@ async function getFiles({ documentRoot = '' }) {
   files['launcher.js'] = await patchLauncher({ documentRoot });
 
   Object.assign(files, {
-    'fastcgi/connection.js': new FileFsRef({ fsPath: require.resolve('fastcgi-client/lib/connection.js') }),
-    'fastcgi/consts.js': new FileFsRef({ fsPath: require.resolve('fastcgi-client/lib/consts.js') }),
-    'fastcgi/stringifykv.js': new FileFsRef({ fsPath: require.resolve('fastcgi-client/lib/stringifykv.js') }),
-    'fastcgi/index.js': new FileFsRef({ fsPath: path.join(__dirname, 'fastcgi/index.js') }),
-    'fastcgi/port.js': new FileFsRef({ fsPath: path.join(__dirname, 'fastcgi/port.js') }),
+    'fastcgi/connection.js': new FileFsRef({
+      fsPath: require.resolve('fastcgi-client/lib/connection.js')
+    }),
+    'fastcgi/consts.js': new FileFsRef({
+      fsPath: require.resolve('fastcgi-client/lib/consts.js')
+    }),
+    'fastcgi/stringifykv.js': new FileFsRef({
+      fsPath: require.resolve('fastcgi-client/lib/stringifykv.js')
+    }),
+    'fastcgi/index.js': new FileFsRef({
+      fsPath: path.join(__dirname, 'fastcgi/index.js')
+    }),
+    'fastcgi/port.js': new FileFsRef({
+      fsPath: path.join(__dirname, 'fastcgi/port.js')
+    })
   });
 
   return files;
@@ -56,24 +74,17 @@ const phpIniData = fs
   .readFileSync(phpIniPath, 'utf-8')
   .replace(/\/root\/app\/native\/modules/g, phpModulesPath);
 
-fs.writeFileSync(
-  path.join(phpNativesPath, 'php-cli.ini'),
-  phpIniData,
-  'utf-8',
-);
+fs.writeFileSync(path.join(phpNativesPath, 'php-cli.ini'), phpIniData, 'utf-8');
 
 async function phpSpawn({ args = [], workPath = phpNativesPath }) {
   return spawnAsync(
     phpPath,
-    [
-      '-c', path.join(phpNativesPath, 'php-cli.ini'),
-      ...args,
-    ],
-    workPath,
+    ['-c', path.join(phpNativesPath, 'php-cli.ini'), ...args],
+    workPath
   );
 }
 
 module.exports = {
   getFiles,
-  phpSpawn,
+  phpSpawn
 };
